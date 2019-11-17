14dd634aa62967c93b6cdf7e5da82780f0617012
Isabella Ramos <isabellamenezesramos@outlook.com>
2019-11-17 11:13:30 -0300

    Create main.c

---
+++ funcaoRecursiva/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+
+    int fatorial(int x);
+    int numero, resultado;
+
+    printf("Digite um número inteiro ");
+    scanf("%i", &numero);
+
+    resultado = fatorial(numero);
+
+    printf("O fatorial é %i\n",resultado);
+
+    return 0;
+}
+
+int fatorial(int x){
+
+    int resultado;
+
+    if(x == 0){
+        resultado = 1;
+    }else{
+        resultado = x * fatorial(x - 1);
+    }
+
+    return resultado;
+
+}
