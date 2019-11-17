a7e14338c840d2f446fc71cc895639fde7aa9dee
Isabella Ramos <isabellamenezesramos@outlook.com>
2019-11-17 11:12:48 -0300

    Create main.c

---
+++ funcoesQueChamamOutrasFuncoes/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+
+    float somaDeDigitos(float num1, float num2); //declarar função
+    float a, b, soma;
+
+    printf("Digite dois números:");
+    scanf("%f", &a);
+    scanf("%f", &b);
+
+    soma = somaDeDigitos(a, b);
+
+    printf("A soma dos dígitos é %f\n",soma);
+
+    return 0;
+}
+
+float somaDeDigitos(float num1, float num2){
+
+    float valorAbsoluto (float x);
+
+    if(num1 < 0){
+        num1 = valorAbsoluto(num1);
+    }
+    if(num2 < 0){
+        num2 =valorAbsoluto(num2);
+    }
+
+    return num1 + num2;
+}
+
+float valorAbsoluto (float x){
+
+    return x * -1;
+
+}
